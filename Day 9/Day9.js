f392b9f7d4aa846deb88e031cc95e008940502f2
SreeBharathiR <bharathisree130@gmail.com>
2025-01-23 20:02:54 +0530

    Day 9

---
+++ Day 9/Day9.js
@@ -0,0 +1,26 @@
+// console.log("Hello...");
+//single comment
+/*multi-line*/
+
+// a = 10;
+// console.log(a);
+// var b = 20;
+// b = "sree";
+// console.log(typeof b);
+// let c = 30;
+// c = "bharathi";
+// console.log(c);
+// const d = 40;
+
+// console.log(typeof d);
+// const boo = true;
+// console.log(true);
+// let sr;
+// console.log(sr);
+
+// arithmetic
+var a = 2;
+var b = 3;
+// var res = a ** b;
+console.log(--a);
+console.log(a);
